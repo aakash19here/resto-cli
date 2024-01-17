19fd205b8ca51abfdcb974db8c4055b715c311de
aakash19here <aakash19here@gmail.com>
2024-01-17 16:42:28 +0530

    feat: cli to create/view files

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import { Command } from "commander";
+import figlet from "figlet";
+import fs from "fs";
+import path from "path";
+
+const program: Command = new Command();
+
+console.log(figlet.textSync("Resto AI"));
+
+program
+  .version("1.0.0")
+  .description("Paste a resto url to scrape the data from it")
+  .option("-l, --ls  [value]", "List directory contents")
+  .option("-m, --mkdir <value>", "Create a directory")
+  .option("-t, --touch <value>", "Create a file")
+  .parse(process.argv);
+
+const options = program.opts();
+
+//define the following function
+async function listDirContents(filepath: string) {
+  try {
+    const files = await fs.promises.readdir(filepath);
+    const detailedFilesPromises = files.map(async (file: string) => {
+      let fileDetails = await fs.promises.lstat(path.resolve(filepath, file));
+      const { size, birthtime } = fileDetails;
+      return { filename: file, "size(KB)": size, created_at: birthtime };
+    });
+
+    const detailedFiles = await Promise.all(detailedFilesPromises);
+    console.table(detailedFiles);
+  } catch (error) {
+    console.error("Error occurred while reading the directory!", error);
+  }
+}
+
+// create a dir
+function createDir(filepath: string) {
+  if (!fs.existsSync(filepath)) {
+    fs.mkdirSync(filepath);
+    console.log("The directory has been created successfully");
+  }
+}
+
+function createFile(filepath: string) {
+  fs.openSync(filepath, "w");
+  console.log("An empty file has been created");
+}
+
+if (options.ls) {
+  const filepath = typeof options.ls === "string" ? options.ls : __dirname;
+  listDirContents(filepath);
+}
+
+// add the following code
+if (options.mkdir) {
+  createDir(path.resolve(__dirname, options.mkdir));
+}
+if (options.touch) {
+  createFile(path.resolve(__dirname, options.touch));
+}
